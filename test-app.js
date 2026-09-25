aee748e9ded025e07244ecdbf3eee59f50e448aa
Erik Björnholm <eb225nm@student.lnu.se>
2026-09-25 12:01:08 +0200

    Implement test app for deck

---
+++ test-app.js
@@ -0,0 +1,7 @@
+import { Card } from './src/Card.js';
+import { Deck } from './src/Deck.js';
+// import { Shuffler } from './src/Shuffler.js';
+
+console.log("New standard deck in a not shuffled order:")
+const deck = new Deck();
+console.log(deck.cards);
